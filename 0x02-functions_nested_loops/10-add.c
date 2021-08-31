0d6114879af5baf7d317d99fedba263e1b2abfa2
mahiET <meazalemma2020@gmail.com>
2021-08-31 08:34:14 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - check the code for ALX School students.
+ * add - adds two numbers together.
+ * @i: The first number to be added.
+ * @j: The second number to be added.
+ * Return: Always 0.
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}

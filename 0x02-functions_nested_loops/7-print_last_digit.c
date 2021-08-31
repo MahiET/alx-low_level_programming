f280a2ce5962c077c6c5e794b5e8e292828eeeb9
mahiET <meazalemma2020@gmail.com>
2021-08-31 07:57:48 -0700

    print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,13 +4,17 @@
  * @n: digit to find the last place of.
  * Return: The last digit.
  */
-int print_last_digit(int n)
+int print_last_digit(int main)
 {
-	if (n < 0)
+
+  int n;
+  if (n < 0)
 		n = n * -1;
 	_putchar((n % 10) + '0');
 	return (n % 10);
 }
+
+
 
 
 

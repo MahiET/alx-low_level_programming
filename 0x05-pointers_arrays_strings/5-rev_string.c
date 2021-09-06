ac5cadb41e39baf1f6911b4a9556eef8b4d71434
mahiET <meazalemma2020@gmail.com>
2021-09-06 05:41:24 -0700

    rev-string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+  /**
+ * rev_string - reverses a string.
+ * @s: input string to reverse.
+ * main - check the code
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char aux;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len--)
+	{
+		aux = s[i];
+		s[i++] = s[len];
+		s[len] = aux;
+	}
+}
+

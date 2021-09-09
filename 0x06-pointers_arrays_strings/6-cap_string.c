3c72341a6b028840708e20c2ef570b18e1ee7f52
mahiET <meazalemma2020@gmail.com>
2021-09-09 06:12:34 -0700

    cap to string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,24 @@
 /**
- * cap_string - a function that capitalizes all words of a string.
- * @s: An input string to capitalize letters
- * main - check the code
+ * cap_string - capitalizes all words of a string.
+ * @s: pointer to input string.
+ * main - chack the code
  * Return: Always 0.
  */
 char *cap_string(char *s)
-{
-	int i = 0;
+  {
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
 
-	while (s[i])
+	i = 1;
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= ('a' - 'A');
+	while (s[i] != '\0')
 	{
-		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-			s[i] -= 32;
-
-		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-			s[i + 1] -= 32;
+		for (j = 0; sep[j] != '\0'; j++)
+			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
+				s[i] -= ('a' - 'A');
 		i++;
 	}
-
 	return (s);
 }
-
 

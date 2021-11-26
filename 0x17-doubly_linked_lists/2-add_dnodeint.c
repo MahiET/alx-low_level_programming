773379364c4cb95e2f2e2feebd46f812d6e1ffc5
MahiET <meazalemma2020@gmail.com>
2021-11-26 00:43:11 -0800

    add dnodeint

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,19 +8,28 @@
   *
   * Return: The new head of the doubly linked list
   */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-  {
-	dlistint_t *tmp;
 
-	tmp = *head;
-	tmp = malloc(sizeof(dlistint_t));
-	if (tmp == NULL)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (head == NULL)
 		return (NULL);
-	tmp->n = n;
-	tmp->prev = NULL;
-	tmp->next = *head;
-	*head = tmp;
-	return (tmp);
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+	return (new);
 }
+
+  
 
 

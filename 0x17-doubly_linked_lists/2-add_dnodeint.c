7f2344f6915de7709e2ec6f42d65b9e169ec4fcd
MahiET <meazalemma2020@gmail.com>
2021-11-26 04:09:36 -0800

    dnodeint git commit -m dnodeint
    clear
    git commit -m dnodeint

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,34 +1,25 @@
 #include "lists.h"
-
 /**
-  * add_dnodeint - Adds a new node at the beginning
-  * of a doubly linked list
-  * @head: The head of the doubly linked list
-  * @n: The number to place in the new node
-  *
-  * Return: The new head of the doubly linked list
+  * add_dnodeint - Adds a node to the beginning of a doubly linked list
+  * @head: head of the linked list
+  * @n: value to add to the list
+  * Return: pointer to the element added
   */
-
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *tmp;
 
-	if (head == NULL)
+	tmp = *head;
+	tmp = malloc(sizeof(dlistint_t));
+	if (tmp == NULL)
 		return (NULL);
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = *head;
-	new->prev = NULL;
-
-	if (*head != NULL)
-		(*head)->prev = new;
-	*head = new;
-	return (new);
+	tmp->n = n;
+	tmp->prev = NULL;
+	tmp->next = *head;
+	*head = tmp;
+	return (tmp);
 }
+
 
   
 

d275074f0526f6ff9f43ea87eb76c7e7348c433b
MahiET <meazalemma2020@gmail.com>
2021-12-03 05:05:41 -0800

    dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -q -O /tmp/libtest.so https://github.com/MitaliSengupta/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libtest.so
+wget -q -O /tmp/libtest.so https://github.com/MahiET/alx-low_level_programming/tree/master/0x18-dynamic_libraries/raw/master/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD=/tmp/libtest.so
+
